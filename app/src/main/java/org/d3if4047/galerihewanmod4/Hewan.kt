b89a71e3ac41dceb11ab24a2af64378e59aca10a
bimo <bimoaghnaa@student.telkomuniversity.ac.id>
2021-05-04 14:38:42 +0700

    Menampilkan gambar dari internet

--- app/src/main/java/org/d3if4047/galerihewanmod4/Hewan.kt
+++ app/src/main/java/org/d3if4047/galerihewanmod4/Hewan.kt
@@ -3,5 +3,5 @@ package org.d3if4047.galerihewanmod4
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int = R.drawable.angsa
+    val imageId: String
 )
--- app/src/main/java/org/d3if4047/galerihewanmod4/ui/main/MainAdapter.kt
+++ app/src/main/java/org/d3if4047/galerihewanmod4/ui/main/MainAdapter.kt
@@ -3,28 +3,35 @@ package org.d3if4047.galerihewanmod4.ui.main
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import org.d3if4047.galerihewanmod4.Hewan
+import org.d3if4047.galerihewanmod4.R
 import org.d3if4047.galerihewanmod4.databinding.ListItemBinding
+import org.d3if4047.galerihewanmod4.network.HewanApi
 
 
-    class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
-        private val data = mutableListOf<Hewan>()
-        fun updateData(newData: List<Hewan>) {
-            data.clear()
-            data.addAll(newData)
-            notifyDataSetChanged()
-        }
-
-    class ViewHolder(private val binding: ListItemBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(hewan: Hewan) = with(binding) {
-            namaTextView.text = hewan.nama
-            latinTextView.text = hewan.namaLatin
-            imageView.setImageResource(hewan.imageResId)
-        }
+class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+    private val data = mutableListOf<Hewan>()
+    fun updateData(newData: List<Hewan>) {
+        data.clear()
+        data.addAll(newData)
+        notifyDataSetChanged()
     }
 
+    class ViewHolder(private val binding: ListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(hewan: Hewan) {
+            with(binding) {
+                namaTextView.text = hewan.nama
+                latinTextView.text = hewan.namaLatin
+                Glide.with(imageView.context)
+                    .load(HewanApi.getHewanUrl(hewan.imageId))
+                    .error(R.drawable.ic_launcher_foreground)
+                    .into(imageView)
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
